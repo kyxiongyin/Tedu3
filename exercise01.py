a018f8bb1266981e02a66e956eb30f569ec16779
Levi <lvze@tedu.cn>
2021-06-16 14:47:06 +0800

    j c

--- exercise01.py
+++ exercise01.py
@@ -6,13 +6,12 @@ def func01():
 def func02():
     sleep(3)
     print('func02 over')
-
+# 传入数据
 pid = os.fork()
 if pid <0:
     print('Error')
 elif pid ==0:
     func01()
-else:
-    func02()
+
 
 
